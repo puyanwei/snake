c488b8b66672148b156e6da26904f3a67ebf354e
Puyan Wei <puyanwei@gmail.com>
2019-10-27 11:07:14 +0000

    Added 2nd useEffect to seperate keypress and new positioning of snake

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,60 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Tile from './Tile.jsx';
+import Tile from "./Tile.jsx";
 
 const Board = ({ rows, cols }) => {
-
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [direction, setDirection] = useState(null)
+    const [direction, setDirection] = useState(null);
 
     useEffect(() => {
         const onKeyPress = (e) => {
-            setInterval(() => {
-                switch (e.keyCode) {
-                    case 38:
-                        console.log("up");
-                        setDirection("up")
-                        if ((position.x - 1) >= 0) {
-                            setPosition({ ...position, x: position.x - 1 })
-                        }
-                        break;
-                    case 40:
-                        setDirection("down")
-                        console.log("down");
-                        if ((position.x + 1) < rows) {
-                            setPosition({ ...position, x: position.x + 1 })
-                        }
-                        break;
-                    case 37:
-                        console.log("left");
-                        setDirection("left")
-                        if ((position.y - 1) >= 0) {
-                            setPosition({ ...position, y: position.y - 1 })
-                        }
-                        break;
-                    case 39:
-                        console.log("right");
-                        setDirection("right");
-                        if ((position.y + 1) < cols) {
-                            setPosition({ ...position, y: position.y + 1 })
-                        }
-                        break;
-
-                    default:
-                        break;
-                }
-            }, 1000)
-        }
+            switch (e.keyCode) {
+                case 38:
+                    return setDirection("up");
+                case 40:
+                    return setDirection("down");
+                case 37:
+                    return setDirection("left");
+                case 39:
+                    return setDirection("right");
+                default:
+                    break;
+            }
+        };
         window.addEventListener("keydown", onKeyPress);
-        return () => window.removeEventListener("keydown", onKeyPress)
+        return () => window.removeEventListener("keydown", onKeyPress);
+    }, []);
 
-    }, [position, cols, rows])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            switch (direction) {
+                case "up":
+                    if (position.x - 1 >= 0) {
+                        setPosition({ ...position, x: position.x - 1 });
+                    }
+                    break;
+                case "down":
+                    if (position.x + 1 < rows) {
+                        setPosition({ ...position, x: position.x + 1 });
+                    }
+                    break;
+                case "left":
+                    if (position.y - 1 >= 0) {
+                        setPosition({ ...position, y: position.y - 1 });
+                    }
+                    break;
+                case "right":
+                    if (position.y + 1 < cols) {
+                        setPosition({ ...position, y: position.y + 1 });
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [direction]);
 
     const style = {
         maxHeight: `${2 * rows}rem`,
         maxWidth: `${2 * cols}rem`,
-        margin: '0 auto',
-        paddingTop: '4rem'
+        margin: "0 auto",
+        paddingTop: "4rem"
     };
 
     const renderBoard = () => {
@@ -62,7 +68,12 @@ const Board = ({ rows, cols }) => {
 
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid[i].length; j++) {
-                grid[i][j] = <Tile isActive={position.x === i && position.y === j} key={`${[i, j]}`} />;
+                grid[i][j] = (
+                    <Tile
+                        isActive={position.x === i && position.y === j}
+                        key={`${[i, j]}`}
+                    />
+                );
             }
         }
         return grid;
